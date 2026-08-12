a567b153fc54b720f80966af7a08909ec807cb4a
Ilyies07 <ilyies.lerhzal@etu.u-paris.fr>
2026-08-13 01:26:26 +0200

    Début de la page pour le 2e projet

--- index.js
+++ index.js
@@ -66,3 +66,34 @@ logosP1.forEach(logop1 => {
     });
 
 });
+
+const descriptionP2 = {
+    cleaning2: "Data cleaning : compréhension du dataset (colonnes, champs, valeurs), gestion des valeurs manquantes, correction des types, suppression des doublons, création de nouvelles colonnes pour simplifier la structure et réduire le nombre de variables, vérification et traitement des valeurs aberrantes.",
+    visualisation2: "Data visualisation : réalisation de 11 visualisations exploratoires avec Matplotlib, Pandas (.plot, .hist) et Seaborn. Les graphiques incluent barplots, histogrammes, courbes et répartitions permettant de répondre à des questions clés : distribution des accidents par heure, proportion par sexe, répartition par âge, influence de la luminosité, de la météo, du type de route et des zones géographiques. Après la création des visualisations, la base a été testée dans SQLite puis importée dans PostgreSQL (via Supabase) afin de vérifier la cohérence des données. Plusieurs requêtes SQL ont été exécutées pour analyser les tendances, valider les résultats observés et renforcer l’interprétation des visualisations.",
+    insight2: "Insights : chaque visualisation a été accompagnée d’une analyse permettant d’interpréter les tendances observées. Les insights mettent en évidence plusieurs points clés : concentration des accidents aux heures de pointe, surreprésentation des hommes dans les accidents corporels, forte implication des jeunes conducteurs, influence de la luminosité et des conditions météo, ainsi que des différences marquées selon les types de routes. Ces observations permettent de mieux comprendre les facteurs de risque et d’orienter l’analyse globale du dataset."
+};
+
+const logosP2 = document.querySelectorAll(".logo-techp2");
+const descBoxP2 = document.getElementById("tool-description");
+
+logosP2.forEach(logop2 => {
+    const tool = logop2.dataset.tool;
+
+    logop2.addEventListener("mouseover", () => {
+        descBoxP2.textContent = descriptionP2[tool];
+    });
+
+    logop2.addEventListener("mouseout", () => {
+        descBoxP2.textContent = "Survolez un outil pour voir les étapes qui ont été effectué ou cliquez sur un outil";
+    });
+
+    logop2.addEventListener("click", () => {
+        const targetId = logop2.dataset.target;
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        }
+    });
+
+});
